dd23af91fc99b666040f5fa1574daf482603455e
GiampyLux <giampaolopaladino01@gmail.com>
2024-06-20 11:18:19 +0200

    Aggiungere i file di progetto.

---
+++ LoginLogout/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace LoginLogout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("Scegli l'operazione da effettuare:");
+            Console.WriteLine("1.: Login");
+            Console.WriteLine("2.: Logout");
+            Console.WriteLine("3.: Verifica Data Login");
+
+            var scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    User.Login();
+                    break;
+                case "2":
+                    User.Logout();
+                    break;
+                case "3":
+                    User.Date();
+                    break;
+            }
+            Menu();
+        }
+    }
+}
---
+++ LoginLogout/User.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginLogout
+{
+    internal class User
+    {
+        public static string username;
+        public static string password;
+        public static bool logged;
+        public static DateTime date;
+        public static void Login()
+        {
+            Console.WriteLine("insericsci username");
+            User.username = Console.ReadLine();
+            Console.WriteLine("inserisci Password");
+            User.password = Console.ReadLine();
+            Console.WriteLine("Conferma Password");
+            string conferma = Console.ReadLine();
+            if (User.password == conferma)
+            {
+                logged = true;
+                date = DateTime.Now;
+                Console.WriteLine("utente loggato Correttamente :)");
+            }
+            else
+            {
+                Console.WriteLine("Impossibile fare il login :(");
+            }
+
+
+        }
+
+        public static void Logout()
+        {
+            if (logged)
+            {
+                username = null;
+                password = null;
+                logged = false;
+                date = default(DateTime);
+                Console.WriteLine("Utente Disconnesso");
+            }
+            else {
+                Console.WriteLine("non c'è nessun utente loggato");
+            }
+        }
+
+        public static void Date()
+        {
+            if (User.logged == true)
+            {
+                Console.WriteLine($"l'utente {User.username} Ha effettuato l'accesso il {User.date}");
+            }
+            else { 
+            Console.WriteLine("non ci sono utenti loggati");}
+
+        }
+    }
+}
